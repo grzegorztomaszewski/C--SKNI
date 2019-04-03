dd0394f46287493ece2a944407d5e2e63e5847fc
grzegorztomaszewski <aroxd3@gmail.com>
2019-04-03 20:40:44 +0200

    z1

---
+++ 1/Pierwszy program/Pierwszy program/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pierwszy_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string zmienna1 = "Cześć";
+            Console.WriteLine(zmienna1);
+            Console.WriteLine("Hello world!");
+
+            //deklaracja
+            short a;
+            int b;
+            double c;
+
+            //inicjalizacja
+            a = 10;
+            b = 20;
+            c = a + b;
+
+            //wypisywanie
+            Console.WriteLine("a= {0}, b={1}, c={2}", a,b,c);
+            Console.WriteLine($"a= {a}, b={b}, c={c}");
+
+            c = a + b;
+            Console.WriteLine($"a+b={c}");
+
+            c = a - b;
+            Console.WriteLine($"a-b={c}");
+
+            c = a * b;
+            Console.WriteLine($"a*b={c}");
+
+            c = a++;
+            Console.WriteLine($"a={a}||c={c}");
+
+            c = ++a;
+            Console.WriteLine($"a={a} || c={c}");
+
+
+            //operatory porównania
+
+            Console.WriteLine("a == b -> {0}", a == b);
+            Console.WriteLine("a > b -> {0}", a > b);
+            Console.WriteLine("a >= b -> {0}", a >= b);
+            */
+
+            string input;
+            Console.WriteLine("Podaj swój wiek: ");
+            input = Console.ReadLine();
+            int wiek = int.Parse(input);
+            if (wiek>20)
+            {
+            Console.WriteLine("Witaj dorosły!");
+            }
+            else
+            {
+                Console.WriteLine("Witaj dzieciaku!");
+            }
+
+            Console.ReadLine(); //komentarz
+        }
+    }
+}
