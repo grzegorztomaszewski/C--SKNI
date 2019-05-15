79898eef2e342906b461009b375c081fe4f47e5a
Grzegorz Tomaszewski <aroxd3@gmail.com>
2019-05-15 19:51:13 +0200

    sprawdzanie wygranej

--- 5/kolkoikrzyzyk/kolkoikrzyzyk/MainPage.xaml.cs
+++ 5/kolkoikrzyzyk/kolkoikrzyzyk/MainPage.xaml.cs
@@ -24,23 +24,81 @@ namespace kolkoikrzyzyk
     {
         public MainPage()
         {
-            this.InitializeComponent();
-            
+            this.InitializeComponent();        
         }
+    }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+    public class Game2
+    {
+        private char[,] gameTab;
+        public char WinnerMark;
+        public Game2()
         {
-
+            gameTab = new char[3, 3]; 
         }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        public bool AddToTabb(int x, int y, char mark)
         {
-
+            if (!Char.IsLetter(gameTab[x,y])) //sprawdza, czy w danym polu znajduję się juz jakiś znak
+            {
+                gameTab[x, y] = mark;
+                return true;
+            }
+            return false;   //jeśli tak, zwraca false
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private bool CheckRows()    //sprawdza poziomo
         {
-
+            char mark = 'X';
+            for (int k = 0; k < 2; k++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (gameTab[i,0] == mark && gameTab[i,1] == mark && gameTab[i,2] == mark)
+                    {
+                        WinnerMark = mark;
+                        return true;
+                    }
+                }
+                mark = 'O';
+            }
+            return false;
         }
+
+        private bool CheckCols()    //sprawdza pionowo
+        {
+            char mark = 'X';
+            for (int k = 0; k < 2; k++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (gameTab[0, i] == mark && gameTab[1, i] == mark && gameTab[2, i] == mark)
+                    {
+                        WinnerMark = mark;
+                        return true;
+                    }
+                }
+                mark = 'O';
+            }
+            return false;
+        }
+        private bool CheckCross()   //sprawdza skosy
+        {
+            char mark = 'X';
+            for (int i = 0; i < 2; i++)
+            {
+                if (gameTab[0, 0] == mark && gameTab[1, 1] == mark && gameTab[2, 2] == mark)
+                {
+                   WinnerMark = mark;
+                   return true;
+                }else if (gameTab[0, 2] == mark && gameTab[1, 1] == mark && gameTab[2, 0] == mark)
+                {
+                   WinnerMark = mark;
+                   return true;
+                }
+                    mark = 'O';
+            }
+            return false;
+        }
+
     }
 }
